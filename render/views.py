652aa203948522b46db2a466727e04ab9dab769a
obed vega <vega.obed@gmail.com>
2023-08-18 18:06:39 -0700

    jsonresponse

--- render/views.py
+++ render/views.py
@@ -1,3 +1,4 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 import json
 from django.http import HttpResponse
@@ -7,10 +8,12 @@ from .utils import obtiene_link
 def index(request):
     return render(request, 'render/index.html', {})
 
+#@api_view(['GET'])
 def check(request):
     resultado = obtiene_link('https://quickstarts.teradata.com/tools-and-utilities/run-bulkloads-efficiently-with-teradata-parallel-transporter.html')
     
     #respuesta = [{'nombre': 'Obed'}]
     json_string = json.dumps(resultado)
-    return HttpResponse(request, json_string)
+    #return HttpResponse(request, json_string)
     #return render(request, respuesta)
+    return JsonResponse(json_string, safe=False)
