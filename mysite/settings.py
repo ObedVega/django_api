10b1e6a9018e4421acfcb136ad348a20b61d5ea5
obed vega <vega.obed@gmail.com>
2023-09-11 14:06:35 -0700

    db

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,10 +94,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    #    'ENGINE': 'django.db.backends.mongoDB',
-    #    'NAME': 'bustedweb',
-    #    'HOST': 'mongodb+srv://saldi:Saldi_1.0@saldi.y8swx.mongodb.net/bustedweb?retryWrites=true&w=majority',  # Cambia a la dirección de tu servidor MongoDB
-    #    'PORT': 27017, 
+        'ENGINE': 'django.db.backends.mongoDB',
+        'NAME': 'bustedweb',
+        'HOST': 'mongodb+srv://saldi:Saldi_1.0@saldi.y8swx.mongodb.net/bustedweb?retryWrites=true&w=majority',  # Cambia a la dirección de tu servidor MongoDB
+        'PORT': 27017, 
     }
 }
 
