bc441b5ab893eeeb9d2661b14eddfcb0c35c4343
obed vega <vega.obed@gmail.com>
2023-09-11 18:01:41 -0700

    djongo

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,12 +94,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
-       'mongodb': {
-        'ENGINE': 'djongo',
-        'NAME': 'bustedweb',
-        'ENFORCE_SCHEMA': True, 
-    },
+    }
 }
 
 
